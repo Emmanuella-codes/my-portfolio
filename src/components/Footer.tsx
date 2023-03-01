411272782846d7097155f7fa659cfdffc1912413
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-03-01 17:57:53 +0100

    fix about me toggle btn, footer and project cmp content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,40 +2,33 @@ import { Box, Text, Flex, Link, Icon } from "@chakra-ui/react";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { RiGithubFill } from "react-icons/ri";
-/* import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react"; */
 
 const Footer = () => {
-  /* useEffect(() => {
-    AOS.init({
-      duration: 2000,
-      easing: "linear",
-      delay: 400,
-    });
-  }, []); */
   return (
     <Box
-      border={{ base: "1px solid #3c6e71", md: "none" }}
-      p={{ base: "4", md: "0" }}
+      border={{ base: "1px dashed #3c6e71", md: "none" }}
+      p={{ base: "4", md: "3" }}
+      borderRadius={"lg"}
       /* data-aos="slide-up" */
     >
-      <Text>Contact me online</Text>
-      <Flex flexDir="row" justifyContent="space-between">
+      <Text fontSize="lg" textAlign={"center"}>
+        Contact me online
+      </Text>
+      <Flex flexDir="row" justifyContent="space-between" mt={3}>
         <Link href="https://twitter.com/Emma_codess" isExternal>
-          <Icon as={AiOutlineTwitter} />
+          <Icon as={AiOutlineTwitter} boxSize={5} />
         </Link>
         <Link
           href="https://www.linkedin.com/in/emmanuella-chukwu-a05b54204/"
           isExternal
         >
-          <Icon as={FaLinkedinIn} />
+          <Icon as={FaLinkedinIn} boxSize={5} />
         </Link>
         <Link
           href="https://www.linkedin.com/in/emmanuella-chukwu-a05b54204/"
           isExternal
         >
-          <Icon as={RiGithubFill} />
+          <Icon as={RiGithubFill} boxSize={5} />
         </Link>
       </Flex>
     </Box>
